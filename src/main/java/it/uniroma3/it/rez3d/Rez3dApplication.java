58428793b01bc9f1424573cad07bb39a3a542aa2
Reziarius <alessandroscoderoni04@gmail.com>
2026-08-22 12:15:39 +0200

    start del progetto

---
+++ src/main/java/it/uniroma3/it/rez3d/Rez3dApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.it.rez3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rez3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rez3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/it/rez3d/Rez3dApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.it.rez3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rez3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
