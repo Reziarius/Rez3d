abd5c569c7e7e5312f0e583cf756bac1de85c4ec
tomm1223 <tommasonolberto@gmail.com>
2026-09-07 17:29:55 +0200

    feat(cart): implement remove line and updateQuantity, added new cart view and style and modified site view (similar to amazon style)

--- src/main/java/it/uniroma3/it/rez3d/controller/CartController.java
+++ src/main/java/it/uniroma3/it/rez3d/controller/CartController.java
@@ -1,25 +1,34 @@
 package it.uniroma3.it.rez3d.controller;
 
+import it.uniroma3.it.rez3d.service.OrderLineService;
 import java.security.Principal;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.it.rez3d.model.Order;
 import it.uniroma3.it.rez3d.model.OrderLine;
 import it.uniroma3.it.rez3d.model.User;
 import it.uniroma3.it.rez3d.service.OrderService;
 import it.uniroma3.it.rez3d.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller 
 public class CartController{
+    private final OrderLineService orderLineService;
     private final OrderService orderService;
     private final UserService userService;
 
-    public CartController(OrderService orderService, UserService userService){
+    public CartController(OrderService orderService, UserService userService, OrderLineService orderLineService){
         this.orderService = orderService;
         this.userService = userService;
+        this.orderLineService = orderLineService;
     }
 
     @GetMapping("/cart")
@@ -41,6 +50,28 @@ public class CartController{
 
         return "cart/showCart";
 
+    }
+
+    @PostMapping("/cart/update/{lineId}")
+    public String aggiornaQuantità(@PathVariable("lineId") Long lineId,@RequestParam("quantity") int nuovaQuantità) {
+        OrderLine line = this.orderLineService.findById(lineId);
+
+        if(line!=null){
+            if(nuovaQuantità<=0){
+                orderLineService.deleteById(lineId);
+            }
+            else{
+                line.setQuantity(nuovaQuantità);
+                orderLineService.save(line);
+            }
+        }
+        return "redirect:/cart";
+    }
+    
+    @PostMapping("/cart/remove/{lineId}")
+    public String aggiornaQuantità(@PathVariable("lineId") Long lineId) {
+        orderLineService.deleteById(lineId);
+        return "redirect:/cart";
     }
     
 }
